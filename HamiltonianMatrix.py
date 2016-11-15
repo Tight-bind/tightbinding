dd2dc64e3685ccad3404f9991ef59e05c14423fc
jackbaker1001 <jsbaker1001@gmail.com>
2016-11-15 17:05:29 +0000

    Cleaned up the sk table. Replaced multiple elif statement with dictionary of Lambda functions

--- HamiltonianMatrix.py
+++ HamiltonianMatrix.py
@@ -1,5 +1,6 @@
 import numpy as np
 from read_coord import AtomicCoordinates
+from slater_koster import on_site_energy_table, slater_koster_table
 
 """
 Author: Jack Baker
@@ -11,101 +12,12 @@ Slater-Koster tables.
 """
 
 
-def eta_coeff(l_i, l_j, bond_type):
-    """
-    string: l_i: The ith atomic orbital angular momentum Q. no (s, p, d etc.)
-    string: l_j: The jth atomic orbital angular momentum Q. no (s, p, d etc.)
-    string: b
-    """
-    eta_string = l_i + "_" + l_j + "_" + bond_type
-    eta_dict = {"s_s_sigma": -1.40,
-                "s_p_sigma": 1.84,
-                "p_s_sigma": 1.84,
-                "p_p_sigma": 3.24,
-                "p_p_pi": -0.81  }
-    if eta_string not in eta_dict:
-        print("Unknown bond orbital combination")
-    else:
-        eta = eta_dict.get(eta_string)
-        return eta
-
-
-def V_coeff(eta, internuclear_distance):
-    """
-    float: eta: Returned from the function eta_coeff (dict look
-           up from solid state table.)
-    float: internuclear_distance: enters function in angstroms, V in eV 
-    """
-    V = 7.619964162248216 * eta * (1/internuclear_distance) ** 2
-    return V
-
-
-def slater_koster_table(orb_i, orb_j, x_dir_cos, y_dir_cos, z_dir_cos, dist_ij):
-    """
-    Calculates the interatomic matrix elements using the Slater-Koster table.
-    string: orb_i, orb_j: ss, px, py, pz...
-    remaining arguments are float64.
-    """
-    if orb_i == "ss" and orb_j == "ss":
-        element = V_coeff(eta_coeff("s", "s", "sigma"), dist_ij)
-
-    elif (orb_i == "ss" and orb_j == "px") or (orb_i == "px" and orb_j == "ss"):
-        element = x_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij)
-
-    elif (orb_i == "ss" and orb_j == "py") or (orb_i == "py" and orb_j == "ss"):
-        element = y_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij)
-
-    elif (orb_i == "ss" and orb_j == "pz") or (orb_i == "pz" and orb_j == "ss"):
-        element = z_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij)
-
-    elif orb_i == "px" and orb_j == "px":
-        element = x_dir_cos ** 2 * V_coeff(eta_coeff("p", "p", "sigma"), dist_ij) +\
-        (1 - x_dir_cos ** 2) * V_coeff(eta_coeff("p", "p", "pi"), dist_ij)
-
-    elif orb_i == "pz" and orb_j == "pz":
-        element = z_dir_cos ** 2 * V_coeff(eta_coeff("p", "p", "sigma"), dist_ij) +\
-        (1 - z_dir_cos ** 2) * V_coeff(eta_coeff("p", "p", "pi"), dist_ij)
-
-    elif orb_i == "py" and orb_j == "py":
-        element = y_dir_cos ** 2 * V_coeff(eta_coeff("p", "p", "sigma"), dist_ij) +\
-        (1 - y_dir_cos ** 2) * V_coeff(eta_coeff("p", "p", "pi"), dist_ij)
-
-    elif (orb_i == "px" and orb_j == "py") or (orb_i == "py" and orb_j == "px"):
-        element = (x_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
-                  (x_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij))
-
-    elif (orb_i == "px" and orb_j == "pz") or (orb_i == "pz" and orb_j == "px"):
-        element = (x_dir_cos * z_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
-                  (x_dir_cos * z_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij))
-
-    elif (orb_i == "pz" and orb_j == "py") or (orb_i == "py" and orb_j == "pz"):
-        element = (z_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
-                  (z_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij))
-    return element
-
-
-def on_site_energy_table(species, orbital):
-    """
-    Some onsite energies for seleted atomic species.
-    string: species: elemental symbol for the species
-    string: orbital: px, py ss...
-    """
-    if species == 'C':
-        if orbital == 'px' or orbital == 'py' or orbital == 'pz':
-            energy = -8.97
-        elif orbital == "ss":
-            energy = -17.52
-    elif species == 'H':
-        energy = -13.61
-    return energy
-
-
 class HamiltonianMatrix(AtomicCoordinates):
     """
     A child class of Atomic_Coordinates used for building and solving
     the tight binding Hamiltonian matrix.
     """
-    
+
     def __init__(self, coord_file, species_dict={}, coords_dict={},
                  orbital_dict={}, atom_id_dict={}):
         AtomicCoordinates.__init__(self, coord_file, species_dict,
@@ -138,11 +50,11 @@ class HamiltonianMatrix(AtomicCoordinates):
                                                       y_dir_cos, x_dir_cos, dist_ij)
                 self.H[i, j] = element
         if show_matrix:
-            print("\n****** Hamiltonian Matrix ******\n")   
+            print("\n****** Hamiltonian Matrix ******\n")
             np.set_printoptions(precision=2)
             print(np.matrix(self.H))
 
-    
+
     def solve_H(self):
         eigen_energies, eigen_vectors = np.linalg.eig(self.H)
         print("\n****** Eigenvalues and Eigenvectors ******* \n")
@@ -150,10 +62,9 @@ class HamiltonianMatrix(AtomicCoordinates):
             print("\nSolution " + str(idx + 1))
             print("Eigenenergy: " + str(eigen_energy) + " eV")
             print("Eigenvector: " + str(eigen_vectors[:,idx]))
-        
-                
+
 if __name__ == "__main__":
-    CH4_matrix = HamiltonianMatrix("CH4.coord")    
-    CH4_matrix.calc_matrix_elements(True, 1.5) 
+    CH4_matrix = HamiltonianMatrix("CH4.coord")
+    CH4_matrix.calc_matrix_elements(True, 1.5)
     CH4_matrix.solve_H()
 
---
+++ slater_koster.py
@@ -0,0 +1,109 @@
+"""
+Author: Jack Baker
+
+Date: 15/11/2016
+
+Description: Some free floating functions and a dictinaory of lambda fucntions used in
+the implementation of the Slater-Koster tables.
+"""
+
+def eta_coeff(l_i, l_j, bond_type):
+    """
+    string: l_i: The ith atomic orbital angular momentum Q. no (s, p, d etc.)
+    string: l_j: The jth atomic orbital angular momentum Q. no (s, p, d etc.)
+    string: b
+    """
+    eta_string = l_i + "_" + l_j + "_" + bond_type
+    eta_dict = {"s_s_sigma": -1.40,
+                "s_p_sigma": 1.84,
+                "p_s_sigma": 1.84,
+                "p_p_sigma": 3.24,
+                "p_p_pi": -0.81  }
+    if eta_string not in eta_dict:
+        print("Unknown bond orbital combination")
+    else:
+        eta = eta_dict.get(eta_string)
+        return eta
+
+
+def V_coeff(eta, internuclear_distance):
+    """
+    float: eta: Returned from the function eta_coeff (dict look
+           up from solid state table.)
+    float: internuclear_distance: enters function in angstroms, V in eV 
+    """
+    V = 7.619964162248216 * eta * (1/internuclear_distance) ** 2
+    return V
+
+
+
+orb_dict = {"ssss": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    V_coeff(eta_coeff("s", "s", "sigma"), dist_ij),
+            "sspx": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    x_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij),
+            "pxss": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    -x_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij),
+            "sspy": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    y_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij),
+            "pyss": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    -y_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij),
+            "sspz": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    z_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij),
+            "pzss": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    -z_dir_cos * V_coeff(eta_coeff("s", "p", "sigma"), dist_ij),
+            "pxpx": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    x_dir_cos ** 2 * V_coeff(eta_coeff("p", "p", "sigma"), dist_ij) +\
+                    (1 - x_dir_cos ** 2) * V_coeff(eta_coeff("p", "p", "pi"), dist_ij),
+            "pzpz": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    z_dir_cos ** 2 * V_coeff(eta_coeff("p", "p", "sigma"), dist_ij) +\
+                    (1 - z_dir_cos ** 2) * V_coeff(eta_coeff("p", "p", "pi"), dist_ij),
+            "pypy": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    y_dir_cos ** 2 * V_coeff(eta_coeff("p", "p", "sigma"), dist_ij) +\
+                    (1 - y_dir_cos ** 2) * V_coeff(eta_coeff("p", "p", "pi"), dist_ij),
+            "pxpy": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    (x_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
+                    (x_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij)),
+            "pypx": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    -1*((x_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
+                    (x_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij))),
+            "pypz": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    (z_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
+                    (z_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij)),
+            "pzpy": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    -1*((z_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
+                    (z_dir_cos * y_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij))),
+            "pzpx": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    (x_dir_cos * z_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
+                    (x_dir_cos * z_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij)),
+            "pxpz": lambda x_dir_cos, y_dir_cos, z_dir_cos, dist_ij:
+                    -1*((x_dir_cos * z_dir_cos *V_coeff(eta_coeff("p", "p", "sigma"), dist_ij)) - \
+                    (x_dir_cos * z_dir_cos *V_coeff(eta_coeff("p", "p", "pi"), dist_ij)))}
+
+def slater_koster_table(orb_i, orb_j, x_dir_cos, y_dir_cos, z_dir_cos, dist_ij):
+    """
+    Calculates the interatomic matrix elements using the Slater-Koster table.
+    string: orb_i, orb_j: ss, px, py, pz...
+    remaining arguments are float64.
+    """
+    orb_string = orb_i + orb_j
+    if orb_string not in orb_dict:
+        print("Orbital overlap %s not recognised" %(orb_string))
+    else:
+        element = orb_dict.get(orb_string)(x_dir_cos, y_dir_cos, z_dir_cos, dist_ij)
+    return element
+
+
+def on_site_energy_table(species, orbital):
+    """
+    Some onsite energies for seleted atomic species.
+    string: species: elemental symbol for the species
+    string: orbital: px, py ss...
+    """
+    if species == 'C':
+        if orbital == 'px' or orbital == 'py' or orbital == 'pz':
+            energy = -8.97
+        elif orbital == "ss":
+            energy = -17.52
+    elif species == 'H':
+        energy = -13.61
+    return energy
