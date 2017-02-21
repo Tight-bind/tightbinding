d94150b7454a291d37bfa1c89e30708910dc082f
Jack Baker <jbaker@petrof.lcn.ucl.ac.uk>
2017-02-21 15:28:45 +0000

    TightBindingParameters class initially finished.

--- get_input_files.py
+++ get_input_files.py
@@ -26,11 +26,34 @@ class TightBindingParameters:
         # Now coordinate file parameters
 
         coord_data = np.genfromtxt(coordinate_file,
-                                   dtype=['U15', '<f8', '<f8', '<f8'],
+                                   dtype=['U15', '<f8', '<f8', '<f8'
+                                          , '<f8', '<f8'],
                                    names=('Species', 'x_coord',
-                                          'y_coord', 'z_coord'),
+                                          'y_coord', 'z_coord',
+                                          'num_orbitals', 'num_valence'),
                                    skip_header=1)
-        print(coord_data)
+        self.num_atoms = len(coord_data)
+        self.coords = np.array([list(coord)[1:-2] for
+                                coord in coord_data],
+                                dtype=np.float)
+        self.species = np.array([coord[0] for
+                                 coord in coord_data],
+                                 dtype=np.str)
+        self.num_orbitals = np.array([coord[4] for
+                                      coord in coord_data],
+                                      dtype=np.int) 
+        self.valence_charges = np.array([coord[5] for
+                                         coord in coord_data],
+                                         dtype=np.int)
+        self.num_electrons = np.sum(self.valence_charges)
+        if isfractionalcoord:
+            self.coords = np.array([coord*self.simcelldims for
+                                    coord in self.coords],
+                                    dtype=np.float)
+        # send everything to input.log
+        with open("input.log", "w") as input_log:
+            for key, item in self.__dict__.items():
+                input_log.write("   =    ".join([str(key), str(item) + "\n"]))
 
 if __name__ == "__main__":
-    TightBindingParameters()
+    params = TightBindingParameters()
